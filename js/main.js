b2ef2b101ab20f9e467165d5cda72d76c1982087
Tero Lompolo <tero.lompolo@gmail.com>
2022-01-09 15:12:47 +0200

    Basic logic to generate message is created and message is displayed to page. Grid model is created and button addec.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-document.getElementById("message").innerHTML = "New text!";
+const library = {
+  energy: ["jang", "jin"],
+  balance: ["strong", "weak", "imbalace", "balance"],
+  action: [
+    "burify your body",
+    "rest",
+    "do vigorous exercises",
+    "study",
+    "bonder your life",
+  ],
+};
+
+const getIndex = (maxLength) => Math.floor(Math.random() * maxLength);
+
+const createMessage = () => {
+  const energy = library.energy[getIndex(library.energy.length)];
+  const balance = library.balance[getIndex(library.balance.length)];
+  const action = library.action[getIndex(library.action.length)];
+  const message = `Your energy is ${energy} and it is ${balance} now. Your should ${action}.`;
+  return message;
+};
+
+document.getElementById("message").innerHTML = createMessage();
